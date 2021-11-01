347c7166d9a88ed19367b652f58e8417f100cb8a
unknown <santhosh8857@gmail.com>
2021-11-01 15:46:07 +0530

    removed unwnated compenents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Section from "./components/Section";
+import Card from "./components/Card";
 
 const App = () => {
-  return <Section />;
+  let free = {
+    subscription: "FREE",
+    price: 0,
+    users: "Single User",
+    storage: "5GB Storage",
+    domain: "Free Subdomain",
+  };
+
+  let plus = {
+    subscription: "PLUS",
+    price: 9,
+    users: "5 Users",
+    storage: "50GB Storage",
+    domain: "Free Subdomain",
+  };
+
+  let pro = {
+    subscription: "PRO",
+    price: 49,
+    users: "Unlimited Users",
+    storage: "150GB Storage",
+    domain: "Unlimited Free Subdomains",
+  };
+
+  return (
+    // Section
+    <section class="pricing py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            {/* Card */}
+            <Card type={free} />
+          </div>
+          <div class="col-lg-4">
+            <Card type={plus} />
+          </div>
+          <div class="col-lg-4">
+            <Card type={pro} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,138 @@
 import React from "react";
 import "../App.css";
-import List from "./List";
+
 const Card = (props) => {
-  let arr = [
-    "Single User",
-    "5GB Storage",
-    "Unlimited Public Projects",
-    "Community Access",
-    "Unlimited Private Projects",
-    "Dedicated Phone Support",
-    "Free Subdomain",
-    "Monthly Status Reports",
-  ];
+  // domain function for rendering domain wrt subscription
+  const domain = (domain) => {
+    if (domain === "FREE")
+      return (
+        <li className="text-muted">
+          <span className="fa-li">
+            <i className="fas fa-times"></i>
+          </span>
+          Free Subdomain
+        </li>
+      );
+    if (domain === "PLUS")
+      return (
+        <li>
+          <span className="fa-li">
+            <i className="fas fa-check"></i>
+          </span>
+          Free Subdomain
+        </li>
+      );
+
+    if (domain === "PRO")
+      return (
+        <li>
+          <span className="fa-li">
+            <i className="fas fa-check"></i>
+          </span>
+          <strong>Unlimited </strong>Free Subdomain
+        </li>
+      );
+  };
   return (
-    // Card
     <div className="card mb-5 mb-lg-0">
-      {/* Card body */}
       <div className="card-body">
+        {/* title */}
         <h5 className="card-title text-muted text-uppercase text-center">
-          {props.type}
+          {props.type.subscription}
         </h5>
+
+        {/* price */}
         <h6 className="card-price text-center">
-          ${props.price}
+          ${props.type.price}
           <span className="period">/month</span>
         </h6>
         <hr />
 
-        <List arr={arr} price={props.price} />
+        {/* List */}
+        <ul className="fa-ul">
+          {props.type.subscription === "FREE" ? (
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.type.users}
+            </li>
+          ) : (
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              <strong>{props.type.users}</strong>
+            </li>
+          )}
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-check"></i>
+            </span>
+            {props.type.storage}
+          </li>
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-check"></i>
+            </span>
+            Unlimited Public Projects
+          </li>
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-check"></i>
+            </span>
+            Community Access
+          </li>
+          {props.type.subscription === "FREE" ? (
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              Unlimited Private Projects
+            </li>
+          ) : (
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              Unlimited Private Projects
+            </li>
+          )}
+          {props.type.subscription === "FREE" ? (
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              Dedicated Phone Support
+            </li>
+          ) : (
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              Dedicated Phone Support
+            </li>
+          )}
+          {domain(props.type.subscription)}
 
+          {props.type.subscription === "PRO" ? (
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              Monthly Status Reports
+            </li>
+          ) : (
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              Monthly Status Reports
+            </li>
+          )}
+        </ul>
         <div className="d-grid">
-          <a href="#" className="btn btn-primary text-uppercase">
-            Button
-          </a>
+          <a className="btn btn-primary text-uppercase">Button</a>
         </div>
       </div>
     </div>
